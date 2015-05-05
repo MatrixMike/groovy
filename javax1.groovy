25e8a7c6584c9159f5e5b81e91361c6a2baa218f
Mike <matrixmike@icloud.com>
2015-05-05 22:51:39 +1000

    adding javax1.groovy

---
+++ javax1.groovy
@@ -0,0 +1,12 @@
+import javax.script.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        ScriptEngineManager factory = new ScriptEngineManager();
+        ScriptEngine engine = factory.getEngineByName("groovy");
+
+        System.out.println(engine.eval("(1..10).sum()"));
+        System.out.println("\nMike");
+  }
+}
